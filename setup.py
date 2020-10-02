29a5c201008daa0d3a43669039c5af756baf6f4b
Андрей Васильевич Верига <veriga@edu.aspu.ru>
2020-10-02 17:30:49 +0400

    'initial'

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+    name='RlGlue',
+    url='https://github.com/andrew-veriga/RL-glue.git',
+    author='AV',
+    author_email='andrew.veriga@gmail.com',
+    #packages=find_packages(),
+    install_requires=[],
+    version=0.1,
+    license='MIT',
+    description='The rl-glue library used in the RL Specialization on Coursera',
+    long_description='todo',
+)
